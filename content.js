0c6ab887e8b252d67a22166ace52e1c9d98f7309
Andrew Grishin <kirivanov777@gmail.com>
2022-02-22 23:21:47 +0300

    fix and optimize update algorithm

--- content.js
+++ content.js
@@ -1,15 +1,25 @@
+function doUpdate() {
+    let event = new Event('click')
+    let elem = document.querySelector('#Pagination > ul > li.page-item.reload > a')
+    elem.dispatchEvent(event)
+}
+
 document.getElementById('submit_button')?.addEventListener('click', () => {
     let intervalId
+    let isFirst = true
 
     function update() {
         let waitingSelects = document.querySelectorAll('option[value="377"][selected="selected"]')
         let allSelects = document.querySelectorAll('option[value="377"]')
-        if (waitingSelects.length > 0 && allSelects.length > 0) {
-            let event = new Event('click')
-            let elem = document.querySelector('#Pagination > ul > li.page-item.reload > a')
-            elem.dispatchEvent(event)
-        } else {
+        let spinner = document.querySelector('#Searchresult > div.spinner-border')
+        console.log(waitingSelects, allSelects, spinner)
+        if (isFirst) {
+            doUpdate()
+            isFirst = false
+        } else if (!spinner && waitingSelects.length == 0 && allSelects.length > 0) {
             clearInterval(intervalId)
+        } else {
+            doUpdate()
         }
     }
 
